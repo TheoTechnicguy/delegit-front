83d41a0a1d1bcdc9f57170974ac6b35e9f37af01
Theo Technicguy <19630890+TheoTechnicguy@users.noreply.github.com>
2024-04-13 18:47:32 +0200

    feat(objects): extend feedback

--- src/lib/objects/feedback.js
+++ src/lib/objects/feedback.js
@@ -5,7 +5,9 @@
  * @abstract The feedback object
  */
 
-import { writable, get } from 'svelte/store';
+import { writable, get } from "svelte/store";
+import api from "../api/feedback.js";
+import Error from "../api/error.js";
 
 class Feedback {
 	/**
@@ -22,7 +24,30 @@ class Feedback {
 		this.Feedback = Feedback;
 		this.Upvotes = writable(Upvotes);
 		this.Downvotes = writable(Downvotes);
-		this.voted = writable(0);
+		this.voteCast = writable(0);
+	}
+
+	/**
+	 * create a new feedback from form data.
+	 * @param {FormData} formData The form data for the new feedback
+	 * @returns {Promise<Feedback>}
+	 */
+	static fromForm(formData) {
+		return new Promise((resolve, reject) => {
+			let course = formData.get("Course")
+			if (course === null) {
+				reject(new Error("Course field not found", "No data was returned when accessing the course field of the form"))
+				return
+			}
+
+			let feedback = formData.get("Feedback")
+			if (feedback === null) {
+				reject(new Error("Feedback field not found", "No data was returned when accessing the feedback field of the form"))
+				return
+			}
+
+			resolve(new Feedback(0, course.toString(), feedback.toString(), 0, 0))
+		})
 	}
 
 	/**
@@ -41,22 +66,60 @@ class Feedback {
 		return get(this.Upvotes) - get(this.Downvotes);
 	}
 
-	upvote() {
-		if (get(this.voted) !== 0) {
-			return;
-		}
-
-		this.Upvotes.update((n) => n + 1);
-		this.voted.set(1);
+	/**
+	 * Serialize the feedback object to a JSON object.
+	 * @returns {object} The feedback object as a JSON object
+	 */
+	toJSON() {
+		return {
+			ID: this.ID,
+			Course: this.Course,
+			Feedback: this.Feedback,
+			Upvotes: get(this.Upvotes),
+			Downvotes: get(this.Downvotes)
+		};
 	}
 
-	downvote() {
-		if (get(this.voted) !== 0) {
+	/**
+	 * Send a positive appreciation to the feedback
+	 * @returns null
+	 */
+	async upvote() {
+		if (get(this.voteCast) !== 0) {
 			return;
 		}
 
-		this.Downvotes.update((n) => n + 1);
-		this.voted.set(-1);
+		api
+			.upvoteFeedback(this.ID)
+			.then(() => {
+				this.Upvotes.update((n) => n + 1);
+				this.voteCast.set(1);
+			})
+			.catch((error) => {
+				// TODO: show error somewhere else than in the console
+				console.error(error);
+			});
+	}
+
+	/**
+	 * Send a negative appreciation to the feedback
+	 * @returns null
+	 */
+	async downvote() {
+		if (get(this.voteCast) !== 0) {
+			return;
+		}
+
+		api
+			.downvoteFeedback(this.ID)
+			.then(() => {
+				this.Downvotes.update((n) => n + 1);
+				this.voteCast.set(-1);
+			})
+			.catch((error) => {
+				// TODO: show error somewhere else than in the console
+				console.error(error);
+			});
 	}
 }
 
